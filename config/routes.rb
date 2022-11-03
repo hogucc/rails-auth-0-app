410e3e0e1b91fb71fa288dfba7ca6b61db7b87b6
hogucc <uku.h1r8@gmail.com>
2022-11-03 10:15:56 +0900

    ログインページへ飛ぶためのページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'top#index'
   get '/auth/auth0/callback' => 'auth0#callback'
   get '/auth/failure' => 'auth0#failure'
   get '/auth/logout' => 'auth0#logout'
